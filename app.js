52ec7d70f6a56b8bb84c8baa883ff4f781b06316
Marc Farra <marcfarra@gmail.com>
2015-12-07 17:56:42 -0500

    Remove app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Scratchpad for testing modularized functions
-
-var fs = require('fs');
-var roadLength = require('./src/metrics/road_length');
-
-var changeset = JSON.parse(fs.readFileSync('test/example.json', 'utf8'));
-
-var roadLengthKM = roadLength(changeset);
-console.log(roadLengthKM);
