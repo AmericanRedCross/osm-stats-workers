47659c40cbec49cc9ed2097776362b389fd390fc
Marc Farra <marcfarra@gmail.com>
2015-12-11 17:23:40 -0500

    Add untagged ways fixtures

--- test/metrics/building_count.test.js
+++ test/metrics/building_count.test.js
@@ -2,17 +2,19 @@ var fs = require('fs');
 var tap = require('tap');
 var buildingCount = require('../../src/metrics/building_count');
 var emptyChangeset = JSON.parse(fs.readFileSync('test/fixtures/null.json'));
-var twoBuildingChangeset = JSON.parse(fs.readFileSync('test/fixtures/buildings/2.json'));
+var untaggedChangeset = JSON.parse(fs.readFileSync('test/fixtures/untagged.json'));
+var twoBuildingChangeset = JSON.parse(fs.readFileSync('test/fixtures/buildings/created_modified.json'));
 
-// An empty changeset should return 0
-tap.test('test empty changeset', function (t) {
-  t.equal(buildingCount(emptyChangeset), 0);
-  t.end();
-});
+tap.test('building_count', function (t) {
+  // An empty changeset should return 0
+  t.equal(buildingCount(emptyChangeset), 0, 'empty changeset should return 0');
 
-// A created and a modified building should both count
-// towards the building count
-tap.test('test building count', function (t) {
-  t.equal(buildingCount(twoBuildingChangeset), 2);
+  // Untagged ways
+  t.equal(buildingCount(untaggedChangeset), 0, 'untagged changeset should return 0');
+
+  // A created and a modified building should both count
+  // towards the building count
+  t.equal(buildingCount(twoBuildingChangeset), 2, 'a created and modified building should return 2');
+
   t.end();
 });
