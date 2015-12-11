16afc9b4add5ae7586f56ad7e6f4bb0f95c753e0
Marc Farra <marcfarra@gmail.com>
2015-12-11 17:24:30 -0500

    Fix reference to source

--- test/metrics/poi_count.test.js
+++ test/metrics/poi_count.test.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var tap = require('tap');
-var poiCount = require('../src/metrics/poi_count');
-var changeset = JSON.parse(fs.readFileSync('fixtures/example.json', 'utf8'));
+var poiCount = require('../../src/metrics/poi_count');
+var changeset = JSON.parse(fs.readFileSync('test/fixtures/example.json', 'utf8'));
 
 tap.test('test POI count', function (t) {
   t.equal(poiCount(changeset), 0);
