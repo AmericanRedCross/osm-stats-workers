d10d3b098cdbcd98c801d570af8ef1b502453409
Marc Farra <marcfarra@gmail.com>
2015-12-14 15:47:25 -0500

    Add fixture for poi count and fix poi count code

--- test/metrics/poi_count.test.js
+++ test/metrics/poi_count.test.js
@@ -1,9 +1,19 @@
 var fs = require('fs');
 var tap = require('tap');
 var poiCount = require('../../src/metrics/poi_count');
-var changeset = JSON.parse(fs.readFileSync('test/fixtures/example.json', 'utf8'));
+var empty = JSON.parse(fs.readFileSync('test/fixtures/null.json', 'utf8'));
+var untagged = JSON.parse(fs.readFileSync('test/fixtures/untagged.json', 'utf8'));
+var createdAmenity = JSON.parse(fs.readFileSync('test/fixtures/poi/created_amenity.json', 'utf8'));
 
-tap.test('test POI count', function (t) {
-  t.equal(poiCount(changeset), 0);
+tap.test('poi_count', function (t) {
+  // Empty changeset
+  t.equal(poiCount(empty), 0, 'empty changeset should return 0');
+
+  // Untagged changeset
+  t.equal(poiCount(untagged), 0, 'untagged changeset should return 0');
+
+  // Created amenity
+  t.equal(poiCount(createdAmenity), 1, 'one created amenity should return 1');
+
   t.end();
 });
