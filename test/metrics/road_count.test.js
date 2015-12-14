f7ac82c47e60b2a3fc5db2de47d7a56a6090e51c
Marc Farra <marcfarra@gmail.com>
2015-12-14 15:48:21 -0500

    Add untagged ways to road count

--- test/metrics/road_count.test.js
+++ test/metrics/road_count.test.js
@@ -2,12 +2,16 @@ var fs = require('fs');
 var tap = require('tap');
 var roadCount = require('../../src/metrics/road_count');
 var empty = JSON.parse(fs.readFileSync('test/fixtures/null.json', 'utf8'));
+var untagged = JSON.parse(fs.readFileSync('test/fixtures/untagged.json', 'utf8'));
 var oneCreatedWay = JSON.parse(fs.readFileSync('test/fixtures/highways/created_way.json', 'utf8'));
 var oneCreatedOneModifedWay = JSON.parse(fs.readFileSync('test/fixtures/highways/created_modified.json', 'utf8'));
 
 tap.test('road_count', function (t) {
   // Empty changeset
   t.equal(roadCount(empty), 0, 'empty changeset should return 0');
+
+  // Untagged changeset
+  t.equal(roadCount(untagged), 0, 'untagged changeset should return 0');
 
   // One created way
   t.equal(roadCount(oneCreatedWay), 1, 'one created way should return 1');
