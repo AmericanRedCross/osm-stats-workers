ac35cba6e663a90e310c906b65306973232a8136
nbumbarger <nbumbarger@gmail.com>
2016-01-07 18:33:01 -0500

    Added logic for sum-related user badge fields, as well as a test

--- src/badges/sum_check.js
+++ src/badges/sum_check.js
@@ -1,71 +1,95 @@
-var badges = {
-  roads: {
-    name: 'Road Builder',
-    id: 1,
-    tiers: {1: 100, 2: 500, 3: 1000}
-  },
-  roadMods: {
-    name: 'Road Maintainer',
-    id: 2,
-    tiers: {1: 100, 2: 500, 3: 1000}
-  },
-  pois: {
-    name: 'Point Creator',
-    id: 3,
-    tiers: {1: 500, 2: 2500, 3: 5000}
-  },
-  buildings: {
-    name: 'Building Builder',
-    id: 4,
-    tiers: {1: 100, 2: 500, 3: 1000}
-  },
-  gpsTraces: {
-    name: 'GPS Trace Creator',
-    id: 5,
-    tiers: {1: 10, 2: 50, 3: 100}
-  },
-  roadKms: {
-    name: 'Long & Winding Road',
-    id: 6,
-    tiers: {1: 50, 2: 100, 3: 500}
-  },
-  roadKmMods: {
-    name: 'Long & Winding Road Maintainer',
-    id: 7,
-    tiers: {1: 50, 2: 100, 3: 500}
-  },
-  waterways: {
-    name: 'Waterway Creator',
-    id: 8,
-    tiers: {1: 50, 2: 100, 3: 500}
-  },
-  countries: {
-    name: 'World Renown',
-    id: 9,
-    tiers: {1: 5, 2: 10, 3: 25}
-  },
-  tasks: {
-    name: 'TaskMan Square Champion',
-    id: 10,
-    tiers: {1: 5, 2: 20, 3: 50}
-  },
-  taskEdits: {
-    name: 'TaskMan Scrutinizer',
-    id: 11,
-    tiers: {1: 5, 2: 20, 3: 50}
-  },
-  josm: {
-    name: 'JOSM User',
-    id: 12,
-    tiers: {1: 1, 2: 10, 3: 100}
-  },
-  hashtags: {
-    name: 'Mapathoner',
-    id: 13,
-    tiers: {1: 5, 2: 20, 3: 50}
-  }
-};
+module.exports = function (data) {
+  var badges = {
+    roads: {
+      name: 'Road Builder',
+      id: 1,
+      tiers: {1: 100, 2: 500, 3: 1000}
+    },
+    roadMods: {
+      name: 'Road Maintainer',
+      id: 2,
+      tiers: {1: 100, 2: 500, 3: 1000}
+    },
+    pois: {
+      name: 'Point Creator',
+      id: 3,
+      tiers: {1: 500, 2: 2500, 3: 5000}
+    },
+    buildings: {
+      name: 'Building Builder',
+      id: 4,
+      tiers: {1: 100, 2: 500, 3: 1000}
+    },
+    gpsTraces: {
+      name: 'GPS Trace Creator',
+      id: 5,
+      tiers: {1: 10, 2: 50, 3: 100}
+    },
+    roadKms: {
+      name: 'Long & Winding Road',
+      id: 6,
+      tiers: {1: 50, 2: 100, 3: 500}
+    },
+    roadKmMods: {
+      name: 'Long & Winding Road Maintainer',
+      id: 7,
+      tiers: {1: 50, 2: 100, 3: 500}
+    },
+    waterways: {
+      name: 'Waterway Creator',
+      id: 8,
+      tiers: {1: 50, 2: 100, 3: 500}
+    },
+    countries: {
+      name: 'World Renown',
+      id: 9,
+      tiers: {1: 5, 2: 10, 3: 25}
+    },
+    tasks: {
+      name: 'TaskMan Square Champion',
+      id: 10,
+      tiers: {1: 5, 2: 20, 3: 50}
+    },
+    taskEdits: {
+      name: 'TaskMan Scrutinizer',
+      id: 11,
+      tiers: {1: 5, 2: 20, 3: 50}
+    },
+    josm: {
+      name: 'JOSM User',
+      id: 12,
+      tiers: {1: 1, 2: 10, 3: 100}
+    },
+    hashtags: {
+      name: 'Mapathoner',
+      id: 13,
+      tiers: {1: 5, 2: 20, 3: 50}
+    }
+  };
 
-Object.keys(badges).forEach(function (key) {
-  console.log(key, badges[key]);
-});
+  function checkBadgeLevel (userTotal, badge) {
+    if (userTotal >= badge.tiers[1] && userTotal < badge.tiers[2]) {
+      return 1;
+    } else if (userTotal >= badge.tiers[2] && userTotal < badge.tiers[3]) {
+      return 2;
+    } else if (userTotal >= badge.tiers[3]) {
+      return 3;
+    } else {
+      return null;
+    }
+  }
+
+  var earnedBadges = {};
+  Object.keys(data).forEach(function (key) {
+    var metric = key;
+    var userTotal = data[key];
+    var badge = badges[key];
+
+    var badgeLevel = checkBadgeLevel(userTotal, badge);
+    if (badgeLevel !== null) {
+      earnedBadges[metric] = badgeLevel;
+    }
+  });
+
+  return earnedBadges;
+};
---
+++ test/badges/sum_stats.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var tap = require('tap');
+var sumCheck = require('../../src/badges/sum_check');
+var sumStats = JSON.parse(fs.readFileSync('test/fixtures/badges/sum_stats.json'));
+
+var userWithNoBadges = sumStats.user_with_no_badges;
+var userWithTierOneBadges = sumStats.user_with_tier1_badges;
+var userWithTierTwoBadges = sumStats.user_with_tier2_badges;
+var userWithTierThreeBadges = sumStats.user_with_tier3_badges;
+
+tap.test('badge logic - sum metrics', function (t) {
+  t.equal(sumCheck(userWithNoBadges), {}, 'a user with all <level 1 metrics should return an empty object');
+
+  t.equal(sumCheck(userWithTierOneBadges), {
+    roads: 1,
+    roadMods: 1,
+    pois: 1,
+    buildings: 1,
+    gpsTraces: 1,
+    roadKms: 1,
+    roadKmMods: 1,
+    waterways: 1,
+    countries: 1,
+    tasks: 1,
+    taskEdits: 1,
+    josm: 1,
+    hashtags: 1
+  }, 'a user with all level 1 metrics should return object of badge keys with all 1s');
+
+  t.equal(sumCheck(userWithTierTwoBadges), {
+    roads: 2,
+    roadMods: 2,
+    pois: 2,
+    buildings: 2,
+    gpsTraces: 2,
+    roadKms: 2,
+    roadKmMods: 2,
+    waterways: 2,
+    countries: 2,
+    tasks: 2,
+    taskEdits: 2,
+    josm: 2,
+    hashtags: 2
+  }, 'a user with all level 2 metrics should return object of badge keys with all 2s');
+
+  t.equal(sumCheck(userWithTierThreeBadges), {
+    roads: 3,
+    roadMods: 3,
+    pois: 3,
+    buildings: 3,
+    gpsTraces: 3,
+    roadKms: 3,
+    roadKmMods: 3,
+    waterways: 3,
+    countries: 3,
+    tasks: 3,
+    taskEdits: 3,
+    josm: 3,
+    hashtags: 3
+  }, 'a user with all level 3 metrics should return object of badge keys with all 3s');
+
+  t.end();
+});
