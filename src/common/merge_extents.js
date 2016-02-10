1f86bb5b0e645e25fec69172a30532591079d6b8
Matthew Hanson <matt.a.hanson@gmail.com>
2016-02-09 19:58:19 -0500

    catch merge error and return original extent

--- src/common/merge_extents.js
+++ src/common/merge_extents.js
@@ -7,8 +7,14 @@ module.exports = function (extent1, extent2) {
   if (typeof extent2 === 'string') {
     extent2 = JSON.parse(extent2);
   }
-  return turf.merge({
-    'type': 'FeatureCollection',
-    'features': [extent1, extent2]
-  });
+  try {
+    return turf.merge({
+      'type': 'FeatureCollection',
+      'features': [extent1, extent2]
+    });
+  } catch (err) {
+    // in case of merge error just return the first extent
+    console.log('Merge error: ', err);
+    return extent1;
+  }
 };
