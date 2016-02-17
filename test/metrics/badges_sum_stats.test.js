8d4924cf38824917569c2b8906a5c0993ea2c22a
nbumbarger <nbumbarger@gmail.com>
2016-02-17 10:14:14 -0500

    Updated tests for dropped road count badges, and did some linting

--- test/metrics/badges_sum_stats.test.js
+++ test/metrics/badges_sum_stats.test.js
@@ -8,12 +8,13 @@ var userWithTierOneBadges = sumStats.user_with_tier1_badges;
 var userWithTierTwoBadges = sumStats.user_with_tier2_badges;
 var userWithTierThreeBadges = sumStats.user_with_tier3_badges;
 
+// note: road counts and road mod counts were removed
 tap.test('badge logic - sum metrics', function (t) {
   t.deepEqual(sumCheck(userWithNoBadges), {}, 'a user with all <level 1 metrics should return an empty object');
 
   t.deepEqual(sumCheck(userWithTierOneBadges), {
-    roads: { category: 1, level: 1 },
-    roadMods: { category: 2, level: 1 },
+    // roads: { category: 1, level: 1 },
+    // roadMods: { category: 2, level: 1 },
     pois: { category: 3, level: 1 },
     buildings: { category: 4, level: 1 },
     gpsTraces: { category: 5, level: 1 },
@@ -28,8 +29,8 @@ tap.test('badge logic - sum metrics', function (t) {
   }, 'a user with all level 1 metrics should return object of badge keys with all 1s');
 
   t.deepEqual(sumCheck(userWithTierTwoBadges), {
-    roads: { category: 1, level: 2 },
-    roadMods: { category: 2, level: 2 },
+    // roads: { category: 1, level: 2 },
+    // roadMods: { category: 2, level: 2 },
     pois: { category: 3, level: 2 },
     buildings: { category: 4, level: 2 },
     gpsTraces: { category: 5, level: 2 },
@@ -44,8 +45,8 @@ tap.test('badge logic - sum metrics', function (t) {
   }, 'a user with all level 2 metrics should return object of badge keys with all 2s');
 
   t.deepEqual(sumCheck(userWithTierThreeBadges), {
-    roads: { category: 1, level: 3 },
-    roadMods: { category: 2, level: 3 },
+    // roads: { category: 1, level: 3 },
+    // roadMods: { category: 2, level: 3 },
     pois: { category: 3, level: 3 },
     buildings: { category: 4, level: 3 },
     gpsTraces: { category: 5, level: 3 },
