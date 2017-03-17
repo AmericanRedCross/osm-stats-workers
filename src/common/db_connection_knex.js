dddb0a8c2156ef97a8efa468ad64c2444a113096
Matthew Hanson <matt.a.hanson@gmail.com>
2017-03-17 11:51:24 -0400

    db migration and seed files moved to osm-stats repo

--- src/common/db_connection_knex.js
+++ src/common/db_connection_knex.js
@@ -1,6 +1,18 @@
 // Returns knex connection to the local
 // missingmaps database
 
-var knexConfig = require('../db/knexfile.js');
-var db = process.env.NODE_ENV || 'development';
-module.exports = require('knex')(knexConfig[db]);
+module.exports = require('knex')({
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: '',
+      database: 'missingmaps'
+    },
+    pool: {
+      min: 0,
+      max: 25
+    },
+    acquireConnectionTimeout: 100000
+});
