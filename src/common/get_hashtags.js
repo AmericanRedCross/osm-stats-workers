abf1b9060a8a3c7cdc7dcc3a5183c9caf34bd0fe
Seth Fitzsimmons <seth@mojodna.net>
2017-09-27 22:59:55 -0700

    Corresponding code is gone

--- src/common/get_hashtags.js
+++ src/common/get_hashtags.js
@@ -1,10 +1,6 @@
 /**
  * Given some text in the form of strings this extracts the #hashtags
  *
- * Worth mentioning is that the .filter is removeing anything that looks
- * like `# hashtag` as opposed to `#hashtag` I wanted to do that with the regex
- * but for the life of me I couldn't figure it out.
- *
  * @param  {String} text This is input text containing #hashtags
  * @return {Array}      A list of all hashtags if the list is empty then there are no hashtags or bad params were passed in.
  */
