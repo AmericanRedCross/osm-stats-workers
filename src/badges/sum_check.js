4bd48e289c130164138542eb815c491f75ece7dc
Seth Fitzsimmons <seth@mojodna.net>
2018-01-24 14:59:56 -0800

    Refactor + fix badge tests

--- src/badges/sum_check.js
+++ src/badges/sum_check.js
@@ -1,91 +1,97 @@
-module.exports = function (data) {
-  var badges = {
-    pois: {
-      name: 'On Point',
-      id: 3,
-      tiers: {1: 500, 2: 2500, 3: 5000}
-    },
-    buildings: {
-      name: 'The Wright Stuff',
-      id: 4,
-      tiers: {1: 100, 2: 500, 3: 1000}
-    },
-    gpsTraces: {
-      name: 'Field Mapper',
-      id: 5,
-      tiers: {1: 10, 2: 50, 3: 100}
-    },
-    roadKms: {
-      name: 'On The Road Again',
-      id: 6,
-      tiers: {1: 50, 2: 100, 3: 500}
-    },
-    roadKmMods: {
-      name: 'Long and Winding Road',
-      id: 7,
-      tiers: {1: 50, 2: 100, 3: 500}
-    },
-    waterways: {
-      name: 'White Water Rafting',
-      id: 8,
-      tiers: {1: 50, 2: 100, 3: 500}
-    },
-    countries: {
-      name: 'World Renown',
-      id: 9,
-      tiers: {1: 5, 2: 10, 3: 25}
-    },
-    tasks: {
-      name: 'Task Champion',
-      id: 10,
-      tiers: {1: 10, 2: 25, 3: 100}
-    },
-    taskValidations: {
-      name: 'Scrutinizer',
-      id: 11,
-      tiers: {1: 25, 2: 100, 3: 250}
-    },
-    josm: {
-      name: 'Awesome JOSM',
-      id: 12,
-      tiers: {1: 1, 2: 10, 3: 100}
-    },
-    hashtags: {
-      name: 'Mapathoner',
-      id: 13,
-      tiers: {1: 5, 2: 20, 3: 50}
-    },
+const BADGES = {
+  pois: {
+    name: "On Point",
+    id: 3,
+    tiers: { 1: 500, 2: 2500, 3: 5000 }
+  },
+  buildings: {
+    name: "The Wright Stuff",
+    id: 4,
+    tiers: { 1: 100, 2: 500, 3: 1000 }
+  },
+  gpsTraces: {
+    name: "Field Mapper",
+    id: 5,
+    tiers: { 1: 10, 2: 50, 3: 100 }
+  },
+  roadKms: {
+    name: "On The Road Again",
+    id: 6,
+    tiers: { 1: 50, 2: 100, 3: 500 }
+  },
+  roadKmMods: {
+    name: "Long and Winding Road",
+    id: 7,
+    tiers: { 1: 50, 2: 100, 3: 500 }
+  },
+  waterways: {
+    name: "White Water Rafting",
+    id: 8,
+    tiers: { 1: 50, 2: 100, 3: 500 }
+  },
+  countries: {
+    name: "World Renown",
+    id: 9,
+    tiers: { 1: 5, 2: 10, 3: 25 }
+  },
+  tasks: {
+    name: "Task Champion",
+    id: 10,
+    tiers: { 1: 10, 2: 25, 3: 100 }
+  },
+  taskValidations: {
+    name: "Scrutinizer",
+    id: 11,
+    tiers: { 1: 25, 2: 100, 3: 250 }
+  },
+  josm: {
+    name: "Awesome JOSM",
+    id: 12,
+    tiers: { 1: 1, 2: 10, 3: 100 }
+  },
+  hashtags: {
+    name: "Mapathoner",
+    id: 13,
+    tiers: { 1: 5, 2: 20, 3: 50 }
+  },
 
-    // ID #14 and 16 used by date check logic
-    taskInvalidations: {
-      name: 'High Standards',
-      id: 16,
-      tiers: {1: 25, 2: 100, 3: 250}
-    }
-  };
+  // ID #14 and 16 used by date check logic
+  taskInvalidations: {
+    name: "High Standards",
+    id: 16,
+    tiers: { 1: 25, 2: 100, 3: 250 }
+  }
+};
 
-  function checkBadgeLevel (userTotal, badge) {
-    if (userTotal >= badge.tiers[1] && userTotal < badge.tiers[2]) {
-      return 1;
-    } else if (userTotal >= badge.tiers[2] && userTotal < badge.tiers[3]) {
-      return 2;
-    } else if (userTotal >= badge.tiers[3]) {
-      return 3;
-    } else {
-      return null;
+function checkBadgeLevel(userTotal, badge) {
+  if (userTotal >= badge.tiers[1] && userTotal < badge.tiers[2]) {
+    return 1;
+  }
+
+  if (userTotal >= badge.tiers[2] && userTotal < badge.tiers[3]) {
+    return 2;
+  }
+
+  if (userTotal >= badge.tiers[3]) {
+    return 3;
+  }
+}
+
+module.exports = data => Object.keys(data).reduce((acc, k) => {
+  const badge = BADGES[k];
+
+  if (badge == null) {
+    return acc;
+  }
+
+  const level = checkBadgeLevel(data[k], badge);
+
+  if (level != null) {
+    acc[k] = {
+      category: badge.id,
+      level
     }
   }
 
-  var earnedBadges = {};
-  Object.keys(data).forEach(function (key) {
-    var userTotal = data[key];
-    var badge = badges[key];
-
-    var badgeLevel = checkBadgeLevel(userTotal, badge);
-    if (badgeLevel !== null) {
-      earnedBadges[key] = {category: badge.id, level: badgeLevel};
-    }
-  });
-
-  return earnedBadges;
-};
+  return acc;
+}, {});
--- test/metrics/badges_sum_stats.test.js
+++ test/metrics/badges_sum_stats.test.js
@@ -1,64 +1,80 @@
-var fs = require('fs');
-var tap = require('tap');
-var sumCheck = require('../../src/badges/sum_check');
-var sumStats = JSON.parse(fs.readFileSync('test/fixtures/badges/sum_stats.json'));
+const tap = require("tap");
 
-var userWithNoBadges = sumStats.user_with_no_badges;
-var userWithTierOneBadges = sumStats.user_with_tier1_badges;
-var userWithTierTwoBadges = sumStats.user_with_tier2_badges;
-var userWithTierThreeBadges = sumStats.user_with_tier3_badges;
+const sumCheck = require("../../src/badges/sum_check");
+const sumStats = require("../fixtures/badges/sum_stats.json");
+
+const userWithNoBadges = sumStats.user_with_no_badges;
+const userWithTierOneBadges = sumStats.user_with_tier1_badges;
+const userWithTierTwoBadges = sumStats.user_with_tier2_badges;
+const userWithTierThreeBadges = sumStats.user_with_tier3_badges;
 
 // note: road counts and road mod counts were removed
-tap.test('badge logic - sum metrics', function (t) {
-  t.deepEqual(sumCheck(userWithNoBadges), {}, 'a user with all <level 1 metrics should return an empty object');
+tap.test("badge logic - sum metrics", t => {
+  t.deepEqual(
+    sumCheck(userWithNoBadges),
+    {},
+    "a user with all <level 1 metrics should return an empty object"
+  );
 
-  t.deepEqual(sumCheck(userWithTierOneBadges), {
-    // roads: { category: 1, level: 1 },
-    // roadMods: { category: 2, level: 1 },
-    pois: { category: 3, level: 1 },
-    buildings: { category: 4, level: 1 },
-    gpsTraces: { category: 5, level: 1 },
-    roadKms: { category: 6, level: 1 },
-    roadKmMods: { category: 7, level: 1 },
-    waterways: { category: 8, level: 1 },
-    countries: { category: 9, level: 1 },
-    tasks: { category: 10, level: 1 },
-    taskEdits: { category: 11, level: 1 },
-    josm: { category: 12, level: 1 },
-    hashtags: { category: 13, level: 1 }
-  }, 'a user with all level 1 metrics should return object of badge keys with all 1s');
+  t.deepEqual(
+    sumCheck(userWithTierOneBadges),
+    {
+      // roads: { category: 1, level: 1 },
+      // roadMods: { category: 2, level: 1 },
+      pois: { category: 3, level: 1 },
+      buildings: { category: 4, level: 1 },
+      gpsTraces: { category: 5, level: 1 },
+      roadKms: { category: 6, level: 1 },
+      roadKmMods: { category: 7, level: 1 },
+      waterways: { category: 8, level: 1 },
+      countries: { category: 9, level: 1 },
+      tasks: { category: 10, level: 1 },
+      // taskEdits: { category: 11, level: 1 },
+      josm: { category: 12, level: 1 },
+      hashtags: { category: 13, level: 1 }
+    },
+    "a user with all level 1 metrics should return object of badge keys with all 1s"
+  );
 
-  t.deepEqual(sumCheck(userWithTierTwoBadges), {
-    // roads: { category: 1, level: 2 },
-    // roadMods: { category: 2, level: 2 },
-    pois: { category: 3, level: 2 },
-    buildings: { category: 4, level: 2 },
-    gpsTraces: { category: 5, level: 2 },
-    roadKms: { category: 6, level: 2 },
-    roadKmMods: { category: 7, level: 2 },
-    waterways: { category: 8, level: 2 },
-    countries: { category: 9, level: 2 },
-    tasks: { category: 10, level: 2 },
-    taskEdits: { category: 11, level: 2 },
-    josm: { category: 12, level: 2 },
-    hashtags: { category: 13, level: 2 }
-  }, 'a user with all level 2 metrics should return object of badge keys with all 2s');
+  t.deepEqual(
+    sumCheck(userWithTierTwoBadges),
+    {
+      // roads: { category: 1, level: 2 },
+      // roadMods: { category: 2, level: 2 },
+      pois: { category: 3, level: 2 },
+      buildings: { category: 4, level: 2 },
+      gpsTraces: { category: 5, level: 2 },
+      roadKms: { category: 6, level: 2 },
+      roadKmMods: { category: 7, level: 2 },
+      waterways: { category: 8, level: 2 },
+      countries: { category: 9, level: 2 },
+      tasks: { category: 10, level: 2 },
+      // taskEdits: { category: 11, level: 2 },
+      josm: { category: 12, level: 2 },
+      hashtags: { category: 13, level: 2 }
+    },
+    "a user with all level 2 metrics should return object of badge keys with all 2s"
+  );
 
-  t.deepEqual(sumCheck(userWithTierThreeBadges), {
-    // roads: { category: 1, level: 3 },
-    // roadMods: { category: 2, level: 3 },
-    pois: { category: 3, level: 3 },
-    buildings: { category: 4, level: 3 },
-    gpsTraces: { category: 5, level: 3 },
-    roadKms: { category: 6, level: 3 },
-    roadKmMods: { category: 7, level: 3 },
-    waterways: { category: 8, level: 3 },
-    countries: { category: 9, level: 3 },
-    tasks: { category: 10, level: 3 },
-    taskEdits: { category: 11, level: 3 },
-    josm: { category: 12, level: 3 },
-    hashtags: { category: 13, level: 3 }
-  }, 'a user with all level 3 metrics should return object of badge keys with all 3s');
+  t.deepEqual(
+    sumCheck(userWithTierThreeBadges),
+    {
+      // roads: { category: 1, level: 3 },
+      // roadMods: { category: 2, level: 3 },
+      pois: { category: 3, level: 3 },
+      buildings: { category: 4, level: 3 },
+      gpsTraces: { category: 5, level: 3 },
+      roadKms: { category: 6, level: 3 },
+      roadKmMods: { category: 7, level: 3 },
+      waterways: { category: 8, level: 3 },
+      countries: { category: 9, level: 3 },
+      tasks: { category: 10, level: 3 },
+      // taskEdits: { category: 11, level: 3 },
+      josm: { category: 12, level: 3 },
+      hashtags: { category: 13, level: 3 }
+    },
+    "a user with all level 3 metrics should return object of badge keys with all 3s"
+  );
 
   t.end();
 });
