93cbb156c688a0f7b8c9d0c84597aa3ee9f40427
Seth Fitzsimmons <seth@mojodna.net>
2018-01-24 15:12:31 -0800

    Refactor sequential date badges

--- src/badges/date_check_sequential.js
+++ src/badges/date_check_sequential.js
@@ -1,68 +1,71 @@
-var R = require('ramda');
+const R = require("ramda");
 
-module.exports = function (dates) {
-  var badges = {
-    daysInRow: {
-      name: 'Consistency',
-      id: 14,
-      tiers: {1: 5, 2: 20, 3: 50}
-    }
-  };
+const BADGES = {
+  daysInRow: {
+    name: "Consistency",
+    id: 14,
+    tiers: { 1: 5, 2: 20, 3: 50 }
+  }
+};
 
-  // function takes array of dates and returns an array of arrays
-  // containing each sequential date
-  // http://stackoverflow.com/questions/16690905/javascript-get-sequential-dates-in-array
-  function sequentializeDates (dates) {
-    // Filter out non-unique dates
-    dates = R.uniq(
-      dates.map(function (date) {
-        date = new Date(date);
-        return date.setHours(0, 0, 0, 0);
-      })
-    );
-
-    var k = 0;
-    var sorted = [];
-    sorted[k] = [];
-    dates.sort(function (a, b) {
-      return +a > +b ? 1 : +a === +b ? 0 : -1;
+// function takes array of dates and returns an array of arrays
+// containing each sequential date
+// http://stackoverflow.com/questions/16690905/javascript-get-sequential-dates-in-array
+function sequentializeDates(dates) {
+  // Filter out non-unique dates
+  const uniqueDates = R.uniq(
+    dates.map(date => {
+      const baseDate = new Date(date);
+      return baseDate.setHours(0, 0, 0, 0);
     })
-    .forEach(function (v, i) {
-      var a = v;
-      var b = dates[i + 1] || 0;
-      sorted[k].push(+a);
-      if ((+b - +a) > 86400000) {
-        sorted[++k] = [];
-      }
-      return 1;
-    });
-    sorted.sort(function (a, b) {
-      return a.length > b.length ? -1 : 1;
-    });
-    return sorted;
-  }
+  );
 
-  function checkBadgeLevel (sequentialDates, badge) {
-    for (var i = 0; i < sequentialDates.length; ++i) {
-      var dayStreakLength = sequentialDates[i].length;
-      if (dayStreakLength >= badge.tiers[1] && dayStreakLength < badge.tiers[2]) {
-        return 1;
-      } else if (dayStreakLength >= badge.tiers[2] && dayStreakLength < badge.tiers[3]) {
-        return 2;
-      } else if (dayStreakLength >= badge.tiers[3]) {
-        return 3;
-      } else {
-        return null;
-      }
+  let k = 0;
+  const sorted = [];
+  sorted[k] = [];
+
+  uniqueDates.sort((a, b) => a - b).forEach((v, i) => {
+    const a = v;
+    const b = uniqueDates[i + 1] || 0;
+    sorted[k].push(+a);
+
+    if (b - a > 86400e3) {
+      sorted[++k] = [];
+    }
+  });
+
+  return sorted.sort((a, b) => a.length - b.length);
+}
+
+function checkBadgeLevel(sequentialDates, badge) {
+  for (let i = 0; i < sequentialDates.length; ++i) {
+    const dayStreakLength = sequentialDates[i].length;
+
+    if (dayStreakLength >= badge.tiers[1] && dayStreakLength < badge.tiers[2]) {
+      return 1;
+    }
+
+    if (dayStreakLength >= badge.tiers[2] && dayStreakLength < badge.tiers[3]) {
+      return 2;
+    }
+
+    if (dayStreakLength >= badge.tiers[3]) {
+      return 3;
     }
   }
+}
 
-  var sequentialDates = sequentializeDates(dates);
-  var badgeLevel = checkBadgeLevel(sequentialDates, badges.daysInRow);
+module.exports = dates => {
+  const sequentialDates = sequentializeDates(dates);
+  const badgeLevel = checkBadgeLevel(sequentialDates, BADGES.daysInRow);
 
-  var earnedBadges = {};
-  if (badgeLevel !== null) {
-    earnedBadges['consistency'] = {category: 14, level: badgeLevel};
+  const earnedBadges = {};
+
+  if (badgeLevel != null) {
+    earnedBadges.consistency = {
+      category: 14,
+      level: badgeLevel
+    };
   }
 
   return earnedBadges;
