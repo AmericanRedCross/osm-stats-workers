cc102479b8039a90d9af01be7ce7c9ae30dde933
Seth Fitzsimmons <seth@mojodna.net>
2018-02-12 16:11:45 -0800

    Refactor housekeeping

--- bin/housekeeping.js
+++ bin/housekeeping.js
@@ -1,32 +1,10 @@
 #!/usr/bin/env node
 
-const async = require("async");
-const env = require("require-env");
-const { Client } = require("pg");
+const housekeeping = require("../src/housekeeping");
+
 
 process.on("unhandledRejection", err => {
   throw err;
 });
 
-const client = new Client(env.require("DATABASE_URL"));
-client.connect();
-
-async.parallel(
-  [
-    async.apply(
-      client.query.bind(client),
-      "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats"
-    ),
-    async.apply(
-      client.query.bind(client),
-      "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
-    )
-  ],
-  err => {
-    if (err) {
-      throw err;
-    }
-
-    client.end();
-  }
-);
+housekeeping();
---
+++ src/housekeeping.js
@@ -0,0 +1,31 @@
+const async = require("async");
+const env = require("require-env");
+const { Client } = require("pg");
+
+const NOOP = () => {};
+
+const client = new Client(env.require("DATABASE_URL"));
+
+module.exports = callback => {
+  callback = callback || NOOP;
+
+  client.connect();
+
+  return async.parallel(
+    [
+      async.apply(
+        client.query.bind(client),
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats"
+      ),
+      async.apply(
+        client.query.bind(client),
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
+      )
+    ],
+    err => {
+      client.end();
+
+      return callback(err);
+    }
+  );
+}
