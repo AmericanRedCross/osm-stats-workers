51ce12c7fed76e3e7fb8119d6e15a1ab441013aa
Seth Fitzsimmons <seth@mojodna.net>
2018-02-15 20:35:34 -0800

    Use connection pooling when housekeeping
    Since functions will re-use a Node process, ending the client prevents
    subsequent invocations from succeeding.

--- src/housekeeping.js
+++ src/housekeeping.js
@@ -1,31 +1,38 @@
 const async = require("async");
 const env = require("require-env");
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
 const { NOOP } = require(".");
 
-const client = new Client(env.require("DATABASE_URL"));
+const pool = new Pool({
+  connectionString: env.require("DATABASE_URL")
+});
 
 module.exports = callback => {
   callback = callback || NOOP;
 
-  client.connect();
-
-  return async.parallel(
-    [
-      async.apply(
-        client.query.bind(client),
-        "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats"
-      ),
-      async.apply(
-        client.query.bind(client),
-        "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
-      )
-    ],
-    err => {
-      client.end();
-
+  return pool.connect((err, client, release) => {
+    if (err) {
+      release();
       return callback(err);
     }
-  );
+
+    return async.parallel(
+      [
+        async.apply(
+          client.query.bind(client),
+          "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats"
+        ),
+        async.apply(
+          client.query.bind(client),
+          "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
+        )
+      ],
+      err => {
+        release();
+
+        return callback(err);
+      }
+    );
+  })
 }
