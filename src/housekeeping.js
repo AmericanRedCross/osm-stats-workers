b99ec382e3e51b3db3b49b93e00a263dff639860
Seth Fitzsimmons <seth@mojodna.net>
2018-03-16 16:27:47 -0700

    Extract pooling boilerplate

--- src/housekeeping.js
+++ src/housekeeping.js
@@ -8,35 +8,44 @@ const pool = new Pool({
   connectionString: env.require("DATABASE_URL")
 });
 
-module.exports = callback => {
-  callback = callback || NOOP;
-
+const query = (query, callback) => {
   return pool.connect((err, client, release) => {
     if (err) {
       release();
       return callback(err);
     }
 
-    return async.parallel(
-      [
-        async.apply(
-          client.query.bind(client),
-          "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats"
-        ),
-        async.apply(
-          client.query.bind(client),
-          "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
-        ),
-        async.apply(
-          client.query.bind(client),
-          "REFRESH MATERIALIZED VIEW CONCURRENTLY raw_hashtags_users"
-        )
-      ],
-      err => {
-        release();
+    return client.query(query, err => {
+      release();
 
-        return callback(err);
-      }
-    );
-  })
-}
+      return callback(err);
+    });
+  });
+};
+
+module.exports = callback => {
+  callback = callback || NOOP;
+
+  return async.parallel(
+    [
+      async.apply(
+        query,
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_stats"
+      ),
+      async.apply(
+        query,
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY raw_countries_users"
+      ),
+      async.apply(
+        query,
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY raw_hashtags_users"
+      ),
+      async.apply(query, "REFRESH MATERIALIZED VIEW CONCURRENTLY user_stats")
+    ],
+    async err => {
+      await pool.end();
+
+      return callback(err);
+    }
+  );
+};
