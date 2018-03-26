44889cb0f7adfe0cfece7e8a616adb42d2c7c15a
Seth Fitzsimmons <seth@mojodna.net>
2018-03-26 15:47:56 -0700

    Run an HTTP server for Web Apps to bind to

--- bin/osm-changes.js
+++ bin/osm-changes.js
@@ -2,6 +2,7 @@
 // to silence EPIPE errors when piping to processes that close the stream ahead
 // of time
 require("epipebomb")();
+const http = require("http");
 
 const stats = require("../src/stats");
 
@@ -12,3 +13,5 @@ process.on("unhandledRejection", err => {
 stats({
   infinite: true
 });
+
+http.createServer((req, res) => res.end("ok")).listen(process.env.PORT || 8080);
