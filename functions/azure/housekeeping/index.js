1826f18ecd0f3b9584bf3b3e1849784c45497bce
Seth Fitzsimmons <seth@mojodna.net>
2018-03-27 10:30:26 -0700

    But it's still a function

--- functions/azure/housekeeping/index.js
+++ functions/azure/housekeeping/index.js
@@ -1,4 +1,4 @@
 const housekeeping = require("../../../src/housekeeping");
 
 exports.handler = context =>
-  housekeeping.then(() => context.done()).catch(err => context.done(err));
+  housekeeping().then(() => context.done()).catch(err => context.done(err));
